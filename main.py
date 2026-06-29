a0d93d4f2e2dc2bb105e6ac5368548b87d08fa40
moataz1411 <mezoahmed1411@gmail.com>
2026-06-30 00:30:16 +0300

    implement bird movement mechanics and update animation frames

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ screen=pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('Flappy Bird')
 ground_scroll=0
 scroll_speed=4
+flying=False
 bg=pygame.image.load('img/bg.png')
 ground_img=pygame.image.load('img/ground.png')
 class Bird(pygame.sprite.Sprite):
@@ -23,11 +24,31 @@ class Bird(pygame.sprite.Sprite):
         self.image=self.images[self.index ]
         self.rect=self.image.get_rect()
         self.rect.center=[x,y]
+        self.vel=0
+        self.clicked=False
+
     def update(self):
+        self.vel+=0.5
+        if self.vel>8:
+            self.vel=8
+        if self.rect.bottom<768:
+            self.rect.y += int(self.vel)
+
+        if pygame.mouse.get_pressed()[0]==1 and self.clicked==False:
+            self.clicked=True
+            self.vel = -10
+        if pygame.mouse.get_pressed()[0]==0:
+            self.clicked=False
+
         self.counter +=1
         flap_cooldown=5
         if self.counter>flap_cooldown:
             self.counter=0
+            self.index+=1
+            if self.index >=len(self.images):
+                self.index=0
+        self.image=self.images[self.index]
+        self.image=pygame.transform.rotate(self.images[self.index],self.vel*-2)
             
 bird_group=pygame.sprite.Group()
 flappy=Bird(100,int(screen_height/2))
@@ -37,6 +58,7 @@ while run:
     clock.tick(fps)
     screen.blit(bg,(0,0))
     bird_group.draw(screen)
+    bird_group.update()
     screen.blit(ground_img,(ground_scroll,768)) 
     ground_scroll-=scroll_speed
     if abs(ground_scroll)>35:
